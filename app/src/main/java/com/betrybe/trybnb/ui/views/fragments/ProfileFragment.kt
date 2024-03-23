14f59e40eb410e170fa6ef315dc5ad9088b20112
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2024-03-23 20:21:09 +0000

    feat: requisito 7

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.google.android.material.textfield.TextInputLayout
 
 class ProfileFragment: Fragment() {
+
+    private lateinit var mInputLogin: TextInputLayout
+    private lateinit var mInputPassword: TextInputLayout
+    private lateinit var mButtonLogin: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +21,36 @@ class ProfileFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        mInputLogin = view.findViewById(R.id.login_input_profile)
+        mInputPassword = view.findViewById(R.id.password_input_profile)
+        mButtonLogin = view.findViewById(R.id.login_input_profile)
+
+        mButtonLogin.setOnClickListener {
+            handleLogin()
+        }
+
         return view
+    }
+
+    private fun handleLogin() {
+        val user = mInputLogin.editText?.text.toString()
+        val pass = mInputPassword.editText?.text.toString()
+
+        isValid(user, pass)
+    }
+
+    private fun isValid(user: String, pass: String) {
+        if (user.isEmpty()) {
+            mInputLogin.error = "O campo Login é obrigatório"
+        } else {
+            mInputLogin.error = null
+        }
+        if (pass.isEmpty()) {
+            mInputPassword.error = "O campo Password é obrigatório"
+        } else {
+            mInputPassword.error = null
+        }
+        //if (user.isNotEmpty() && pass.isNotEmpty()) {
+        //}
     }
 }
